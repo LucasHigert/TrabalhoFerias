c6a40764a758aae00123f8700b9c2d4ac13eeb22
heloisa <heloisasmengisztki@gmail.com>
2019-07-27 14:03:39 -0300

    Editando model/Cidades

---
+++ Model/Cidade.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+   public class Cidade
+    {
+        public int Id;
+        public string Nome;
+        public string Cpf;
+        
+
+
+        //Propriedade para a coluna do id_Estado(FK)
+        public int IdEstado;
+
+        /*Objeto da estado que permitirá acessar as 
+         * informações de estado através da cidade.
+         */
+        public Estado Estado;
+    }
+}
--- Repositorio/Estrutura.sql
+++ Repositorio/Estrutura.sql
@@ -15,6 +15,25 @@ CREATE TABLE estados(
 	nome VARCHAR (50),
 	sigla VARCHAR (2)
 	);
+	
+	CREATE TABLE cidades(
+	id INT PRIMARY KEY IDENTITY(1,1),
+
+	id_estado INT,
+	FOREIGN KEY(id_estado) REFERENCES estados(id),
+
+	nome VARCHAR (50),
+	cpf VARCHAR (50),
+	data_nascimento DATETIME2(7),
+	numero INT,
+	complemento NCHAR(10),
+	logradouro NCHAR(10),
+	cep NCHAR(10)
+	);
 
 
 INSERT INTO estados (nome, sigla) VALUES ('heloisa', 'Sc');
+
+SELECT cidades.id AS 'CidadeId'
+FROM cidades INNER JOIN estados ON (cidades.id_estado=estados.id);
+
